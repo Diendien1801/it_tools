5185c3a3ced77cd202e545ae8b9eec4d397f4638
Doan <nguyenquangdoan27092004@gmail.com>
2025-04-17 23:42:16 +0700

    feat: Cac chuc nang nhom Math

--- it_tools/obj/x64/Debug/net6.0-windows10.0.19041.0/win10-x64/it_tools.AssemblyInfo.cs
+++ it_tools/obj/x64/Debug/net6.0-windows10.0.19041.0/win10-x64/it_tools.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("it_tools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+693efaef9fcab9069f19a196fc174c58e57d5d9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3aade6cd27f421be020ad8345c4fb0b10f8ffa9")]
 [assembly: System.Reflection.AssemblyProductAttribute("it_tools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("it_tools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
